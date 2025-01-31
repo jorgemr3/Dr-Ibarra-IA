cac4593cd54130714fa09ccda4b13995f1443247
jorgemr3 <jorgemr103@gmail.com>
2025-01-31 12:52:37 -0600

    Refactor dataset generation and enhance distance calculation methods in Programa4_v2.py

--- Programa4_v2.py
+++ Programa4_v2.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np 
 
 try: 
     with open("dataset.txt", "r") as file:
@@ -19,7 +20,7 @@ prueba = random.sample(dataset, num_prueba)
 # Seleccionar el 70% de los datos restantes
 entrenamiento = [fila for fila in dataset if fila not in prueba]
 
-confusion = [[0, 0], [0, 0]] # verdadero positivo, falso positivo, verdadero negativo, falso negativo
+confusion = np.zeros((2,2)) # verdadero positivo, falso positivo, verdadero negativo, falso negativo
 vp = confusion[0,0]
 fp = confusion[0,1]
 vn = confusion[1,0]
@@ -28,3 +29,42 @@ R_manhattan = []
 R_euclidiana = []
 
 
+precision = np.zeros((6, 3))
+
+
+
+for fila in prueba:
+    # Calcular la distancia Manhattan
+    distancias_manhattan = []
+    for fila_entrenamiento in entrenamiento:
+        distancia = sum([abs(fila[i] - fila_entrenamiento[i]) for i in range(1, len(fila) - 1)])
+        distancias_manhattan.append((fila_entrenamiento[0], distancia))
+    distancias_manhattan.sort()
+    # guardar en distancias manhatan indice , valor 
+    print(distancias_manhattan[0][1]) 
+    
+    R_manhattan.append(distancias_manhattan[0][1])
+    # Calcular la distancia Euclidiana
+    distancias_euclidiana = []
+    for fila_entrenamiento in entrenamiento:
+        distancia = sum([(fila[i] - fila_entrenamiento[i]) ** 2 for i in range(1, len(fila) - 1)]) ** 0.5
+        distancias_euclidiana.append((fila_entrenamiento[0],distancia))
+    distancias_euclidiana.sort()
+    
+    print(distancias_euclidiana[0][1])
+    
+    R_euclidiana.append(distancias_euclidiana[0][1])
+    # Calcular la precisión
+    if fila[-1] == R_manhattan[-1]:
+        if fila[-1] == 1:
+            vp += 1
+        else:
+            vn += 1
+    else:
+        if fila[-1] == 1:
+            fn += 1
+        else:
+            fp += 1
+    # precision[0][0].append() # verdaderos pos - falsos neg entre los de prueba
+    
+    
--- matlab/Programa3_generador.py
+++ matlab/Programa3_generador.py
@@ -1,6 +1,6 @@
 import random
 
 with open("dataset.txt", "w") as file:
-    for _ in range(1000):
-        fila = [random.randint(0, 10) for _ in range(6)] + [random.randint(1, 2)]
+    for i in range(1000):
+        fila = [i +1] + [random.randint(0, 10) for _ in range(6)] + [random.randint(1, 2)]
         file.write(" ".join(map(str, fila)) + "\n")
